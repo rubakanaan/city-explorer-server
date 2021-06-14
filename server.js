f0afc2527b60bc6c707fbc10c13bed65902805eb
root <root@STDLTUC-L069.localdomain>
2021-06-15 01:46:37 +0300

    fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const data = require('./data/weather.json');
 // a server endpoint 
 app.get('/', // our endpoint name
  function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World!!!!!') // our endpoint function response
+  res.send('Hello World!') // our endpoint function response
 });
 app.get('/weather-data',(req,res)=>{
     res.json(data);
