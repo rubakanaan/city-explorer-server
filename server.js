b8aa1db7900c280a154a97f7dcbbb0d9f77ab6e2
root <root@STDLTUC-L069.localdomain>
2021-06-15 23:35:08 +0300

    lab 8

--- server.js
+++ server.js
@@ -5,26 +5,47 @@ app.use(cors()) // after you initialize your express app instance
 require('dotenv').config();
 
 
-const port= process.env.PORT;
+const port = process.env.PORT;
+const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
 const wData = require('./data/weather.json');
+const axios= require('axios');
+const { response } = require('express');
 // a server endpoint 
-app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World!') // our endpoint function response
+app.get('/',
+  function (req, res) {
+    res.send('Hello World!')
+  });
+app.get('/weather', (req, res) => {
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+  console.log(lat);
+  if (lat && lon) {
+    const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`
+   
+    axios.get(weatherUrl).then(response =>{
+      const respData = response.data.data.map(obj => new Weather(obj));
+
+     res.json(respData)
+   }).catch(error=>{
+     res.send(error.message );
+   });
+    console.log(weatherUrl);
+  }
+  else {
+    res.send('please provide the proper lat and lon ');
+  }
+  
+  
 });
-app.get('/weather',(req,res)=>{
-  const respData=wData.data.map(obj => new Weather(obj));
-    res.json(respData);
-});
-class Weather{
-  constructor(wData){
-    this.description=wData.weather.description;
-    this.date=wData.valid_date;
-    this.high=wData.max_temp;
-    this.low=wData.low_temp;
+class Weather {
+  constructor(wData) {
+    this.description = wData.weather.description;
+    this.date = wData.valid_date;
+    this.high = wData.max_temp;
+    this.low = wData.low_temp;
   }
 }
-app.listen(port, () =>{
+app.listen(port, () => {
   console.log(`server start on port: ${port}`);
 }
 ); // kick start the express server to work
