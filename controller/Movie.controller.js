f0e347a8475f6136ab1da411cc59d7e216292113
root <root@STDLTUC-L069.localdomain>
2021-06-19 21:20:57 +0300

    add cache

--- controller/Movie.controller.js
+++ controller/Movie.controller.js
@@ -1,22 +1,53 @@
 const Movie = require('../models/Movie.model');
 const axios = require('axios');
+const Cache = require('../helper/cache')
+
 require('dotenv').config();
 
 
 const MOVIE_KEY = process.env.MOVIE_KEY;
+const cacheObj = new Cache();
 
 const movieController = (req, res) => {
 
-    const search = req.query.query;
-    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_KEY}&query=${search}`;
-    console.log(movieUrl);
+    const cityName = req.query.query;
 
-    axios.get(movieUrl).then(response => {
-        const result = response.data.results.map(obj => new Movie(obj));
-        res.json(result)
-    }).catch(error => {
-        res.send(error.message);
-    });
+    const requestKey = `movie-${cityName}`;
+    console.log(cacheObj[requestKey]);
+    console.log(requestKey);
+
+
+
+
+    if (cityName) {
+        if (cacheObj[requestKey]) {
+            console.log('--------------------');
+            console.log('From Movie cache obj');
+            console.log('--------------------');
+            res.json(cacheObj[requestKey]);
+        }
+
+        else {
+
+            const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_KEY}&query=${cityName}`;
+            console.log(movieUrl);
+            axios.get(movieUrl).then(response => {
+                const result = response.data.results.map(obj => new Movie(obj));
+                res.json(result)
+                cacheObj[requestKey] = result;
+                console.log('--------------------');
+                console.log('From Movie axios');
+                console.log('--------------------');
+                res.json(respData)
+            }).catch(error => {
+                res.send(error.message);
+            });
+
+        }
+    }
+    else {
+        res.send('please provide the proper lat and lon ');
+    }
 
 
 }
--- controller/Weather.controller.js
+++ controller/Weather.controller.js
@@ -1,24 +1,41 @@
 const Weather = require('../models/Weather.model');
 const axios = require('axios');
 const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
-//const wData = require('./data/weather.json');
+const Cache = require('../helper/cache')
 require('dotenv').config();
 
+const cacheObj = new Cache();
 const weatherController = (req, res) => {
     const lat = req.query.lat;
     const lon = req.query.lon;
-    console.log(lat);
+    const requestKey = `weather-${lat}-${lon}`
+    console.log(cacheObj[requestKey]);
+    console.log(requestKey);
+
+    
     if (lat && lon) {
-        const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`
+        if (cacheObj[requestKey]) {
+            console.log('--------------------');
+            console.log('From Weather cache obj');
+            console.log('--------------------');
+            res.json(cacheObj[requestKey]);
+        }
 
-        axios.get(weatherUrl).then(response => {
-            const respData = response.data.data.map(obj => new Weather(obj));
+        else {
+            const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`
 
-            res.json(respData)
-        }).catch(error => {
-            res.send(error.message);
-        });
-        console.log(weatherUrl);
+            axios.get(weatherUrl).then(response => {
+                const respData = response.data.data.map(obj => new Weather(obj));
+                cacheObj[requestKey] = respData;
+                console.log('--------------------');
+                console.log('From Weather axios');
+                console.log('--------------------');
+                res.json(respData)
+            }).catch(error => {
+                res.send(error.message);
+            });
+            console.log(weatherUrl);
+        }
     }
     else {
         res.send('please provide the proper lat and lon ');
